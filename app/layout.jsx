e2cdcdf16905e480598ea8120fdd0abd1460b331
Sidd_ <siddharth726@outlook.com>
2024-01-06 12:32:09 -0800

    [FIX] searching photos done

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,6 +11,21 @@ export const metadata = {
   description: "I code, take photos and livestream.",
 };
 
+export function TailwindIndicator() {
+  if (process.env.NODE_ENV === "production") return null
+
+  return (
+    <div className="fixed bottom-1 left-1 z-50 flex h-6 w-6 items-center justify-center rounded-full bg-gray-800 p-3 font-mono text-xs text-white">
+      <div className="block sm:hidden">xs</div>
+      <div className="hidden sm:block md:hidden">sm</div>
+      <div className="hidden md:block lg:hidden">md</div>
+      <div className="hidden lg:block xl:hidden">lg</div>
+      <div className="hidden xl:block 2xl:hidden">xl</div>
+      <div className="hidden 2xl:block">2xl</div>
+    </div>
+  )
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -25,9 +40,12 @@ export default function RootLayout({ children }) {
         >
           <Navbar />
 
-          <main className="w-full max-w-5xl">{children}</main>
+          <main className="w-full max-w-5xl min-h-screen">{children}</main>
           <Footer />
         </ThemeProvider>
+
+        {/* For screen sizes */}
+        <TailwindIndicator />
       </body>
     </html>
   );
--- components/photos/gallery.jsx
+++ components/photos/gallery.jsx
@@ -5,44 +5,73 @@ import { Input } from "@/components/ui/input";
 import { Photo } from "@/components/photos/photo";
 import originalPhotosArray from "@/lib/photos.json";
 import { useState, useEffect } from "react";
+import { Loading } from "../ui/loading";
+import { until } from "@/lib/utils";
 
 var temp1 = [];
 var temp2 = [];
 
-for (let i = 0; i < originalPhotosArray.length; i += 1) {
-  if (i % 2 === 0) {
-    temp1.push(originalPhotosArray[i]);
-  } else {
-    temp2.push(originalPhotosArray[i]);
+function initializePhotos() {
+  for (let i = 0; i < originalPhotosArray.length; i += 1) {
+    if (i % 2 === 0) {
+      temp1.push(originalPhotosArray[i]);
+    } else {
+      temp2.push(originalPhotosArray[i]);
+    }
   }
 }
 
 export function Gallery() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [searchIcon, setSearchIcon] = useState(<MagnifyingGlassIcon />);
   const [photos1, setPhotos1] = useState(temp1);
   const [photos2, setPhotos2] = useState(temp2);
+  const [searchError, setSearchError] = useState(false);
 
-  function filterPhotos(photos, query) {
+  async function filterPhotos() {
+    let photos = [...originalPhotosArray];
+    let query = searchQuery;
+
     let anotherTemp1 = [];
     let anotherTemp2 = [];
+
+    var doneWithSearching = false;
+    var doneWithArrays = false;
 
     for (let i = photos.length - 1; i >= 0; i--) {
       let found = false;
       for (let key in photos[i]) {
-        console.log(photos[i][key]);
         if (Array.isArray(photos[i][key])) {
+          for (let morekey in photos[i][key]) {
+            if (photos[i][key][morekey].toLowerCase().includes(query)) {
+              found = true;
+              if (i === photos.length - 1) doneWithSearching = true;
+              break;
+            }
+          }
         } else if (typeof photos[i][key] === "number") {
-        } else {
-          if (photos[i][key].includes(query)) {
+          if (photos[i][key].toString().includes(query)) {
             found = true;
+            if (i === photos.length - 1) doneWithSearching = true;
+            break;
+          }
+        } else {
+          if (photos[i][key].toLowerCase().includes(query)) {
+            found = true;
+            if (i === photos.length - 1) doneWithSearching = true;
             break;
           }
         }
+
+        if (i === photos.length - 1) doneWithSearching = true;
       }
       if (!found) {
         photos.splice(i, 1);
       }
     }
+
+    if (photos.length === 0) return setSearchError(true);
+    else setSearchError(false);
 
     for (let i = 0; i < photos.length; i += 1) {
       if (i % 2 === 0) {
@@ -50,24 +79,32 @@ export function Gallery() {
       } else {
         anotherTemp2.push(photos[i]);
       }
+
+      if (i === photos.length - 1) doneWithArrays = true;
     }
 
     setPhotos1(() => anotherTemp1);
     setPhotos2(() => anotherTemp2);
+
+    await until((_) => doneWithSearching === true && doneWithArrays === true);
+    setSearchIcon(<MagnifyingGlassIcon />);
   }
 
   useEffect(() => {
-    const timeOutId = setTimeout(
-      () => filterPhotos([...photos1, ...photos2], searchQuery),
-      500,
-    );
+    initializePhotos();
+    setSearchIcon(<Loading />);
+
+    const timeOutId = setTimeout(() => {
+      console.log("ok");
+      filterPhotos();
+    }, 500);
     return () => clearTimeout(timeOutId);
   }, [searchQuery]);
 
   return (
     <div className="my-6">
       <div className="pointer-events-none flex items-center rounded-lg border border-zinc-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors hover:border-zinc-300 hover:ring-zinc-950 dark:border-zinc-800 dark:hover:border-zinc-700 dark:hover:ring-zinc-300">
-        <MagnifyingGlassIcon />
+        {searchIcon}
         <Input
           onChange={(event) => setSearchQuery(event.target.value)}
           className="pointer-events-auto !border-transparent !bg-transparent pr-16 !ring-0"
@@ -75,17 +112,25 @@ export function Gallery() {
         />
       </div>
 
-      <div className="mt-4 flex gap-4">
-        <div className="flex w-1/2 flex-col space-y-4">
-          {photos1.map((photo) => (
-            <Photo key={photo.name} photoData={photo} />
-          ))}
-        </div>
-        <div className="flex w-1/2 flex-col space-y-4">
-          {photos2.map((photo) => (
-            <Photo key={photo.name} photoData={photo} />
-          ))}
-        </div>
+      <div className="mt-4 flex w-full gap-4">
+        {searchError === false ? (
+          <>
+            <div className="flex w-1/2 flex-col space-y-4">
+              {photos1.map((photo) => (
+                <Photo key={photo.name} photoData={photo} />
+              ))}
+            </div>
+            <div className="flex w-1/2 flex-col space-y-4">
+              {photos2.map((photo) => (
+                <Photo key={photo.name} photoData={photo} />
+              ))}
+            </div>
+          </>
+        ) : (
+          <p className="w-full py-4 text-center text-sm text-zinc-400">
+            No photos found... Try clearing some search filters?
+          </p>
+        )}
       </div>
     </div>
   );
