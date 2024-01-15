9156e76d0e3346ea3484617c4c9d3f42906a06bd
Sidd_ <siddharth726@outlook.com>
2024-01-15 15:40:59 -0800

    [CHORE] capitalization

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -23,7 +23,7 @@ export default function NotFound() {
           className="mt-0.5 w-fit flex items-center border-b border-zinc-400 text-sm transition-all hover:border-zinc-500 dark:border-white/25 dark:text-zinc-300 dark:hover:border-white/50 dark:hover:text-zinc-50"
           href="/"
         >
-          Return Home?
+          return home?
         </Link>
       </div>
     </div>
