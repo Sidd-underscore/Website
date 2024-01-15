c77f21b820761a05b1462d2dbb721599730ea0af
Sidd_ <siddharth726@outlook.com>
2024-01-15 11:04:05 -0800

    [FIX] maintenance

--- components/home/footer.jsx
+++ components/home/footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "@/components/ui/link";
 import { useState } from "react";
 
 export function Footer() {
-  const [hearts, setHearts] = useState([]);
+  const [heart, setHeart] = useState(null);
   const [canSpawn, setCanSpawn] = useState(true);
 
   const handleHeartHover = (e) => {
@@ -12,19 +12,16 @@ export function Footer() {
       const newHeart = {
         id: Date.now(),
         x: e.clientX,
-        y: e.clientY,
+        y: e.clientY + window.scrollY - 25,
       };
 
-      setHearts((prevHearts) => [...prevHearts, newHeart]);
+      setHeart(newHeart);
       setCanSpawn(false);
 
       setTimeout(() => {
-        setHearts((prevHearts) =>
-          prevHearts.filter((heart) => heart.id !== newHeart.id),
-        );
-      }, 10000);
-
-      setTimeout(() => setCanSpawn(true), 100);
+        setHeart(null);
+        setCanSpawn(true);
+      }, 1000);
     }
   };
 
@@ -32,7 +29,10 @@ export function Footer() {
     <>
       <footer className="p-4 text-center">
         <sub>
-          Made with Next.js and <span onMouseEnter={handleHeartHover}>love</span>{" "}
+          Made with Next.js and{" "}
+          <span className="cursor-text select-none" onClick={handleHeartHover}>
+            love
+          </span>{" "}
           by Sidd in Portland, OR.
           <p>
             <Link
@@ -58,18 +58,24 @@ export function Footer() {
         </sub>
       </footer>
 
-      {hearts.map((heart) => (
-        <span
-          key={heart.id}
-          className="love heart-animation"
-          style={{
-            left: heart.x,
-            top: heart.y + Math.floor(Math.random() * (501 - 451) + 450),
-          }}
-        >
-          ❤️
-        </span>
-      ))}
+      {heart &&
+        (() => {
+          var random = Math.floor(Math.random() * 27);
+          return (
+            <span
+              key={heart.id}
+              className={`love ${
+                random % 2 === 0 ? "heart-animation-right" : "heart-animation-left"
+              }`}
+              style={{
+                left: heart.x,
+                top: heart.y,
+              }}
+            >
+              {random % 4 === 0 ? "💖" : "❤️"}
+            </span>
+          );
+        })()}
     </>
   );
 }
