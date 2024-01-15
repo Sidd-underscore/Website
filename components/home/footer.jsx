7a718589e2328695d4caa1d2ec0ad8a36f027c03
Sidd_ <siddharth726@outlook.com>
2024-01-14 16:05:43 -0800

    [FEAT] nothing to see here

--- components/home/footer.jsx
+++ components/home/footer.jsx
@@ -1,20 +1,75 @@
+"use client";
+
 import { Link } from "@/components/ui/link";
+import { useState } from "react";
 
 export function Footer() {
+  const [hearts, setHearts] = useState([]);
+  const [canSpawn, setCanSpawn] = useState(true);
+
+  const handleHeartHover = (e) => {
+    if (canSpawn) {
+      const newHeart = {
+        id: Date.now(),
+        x: e.clientX,
+        y: e.clientY,
+      };
+
+      setHearts((prevHearts) => [...prevHearts, newHeart]);
+      setCanSpawn(false);
+
+      setTimeout(() => {
+        setHearts((prevHearts) =>
+          prevHearts.filter((heart) => heart.id !== newHeart.id),
+        );
+      }, 10000);
+
+      setTimeout(() => setCanSpawn(true), 50);
+    }
+  };
+
   return (
-    <footer className="text-center p-4">
-      <sub>
-        Made with Next.js and love by Sidd in Portland, OR.{" "}
-        <p>
-          <Link
-            className="text-xs inline-flex dark:text-zinc-50"
-            href="/photography/PXL_20230903_140052596.PORTRAIT.jpg"
-          >
-            Ivy
-          </Link>{" "}
-          says hi!
-        </p>
-      </sub>
-    </footer>
+    <>
+      <footer className="p-4 text-center">
+        <sub>
+          Made with Next.js and <span onMouseMove={handleHeartHover}>love</span>{" "}
+          by Sidd in Portland, OR.
+          <p>
+            <Link
+              className="inline-flex text-xs dark:text-zinc-50"
+              href="/images/i.jpg"
+            >
+              I
+            </Link>
+            <Link
+              className="inline-flex text-xs dark:text-zinc-50"
+              href="/images/v.jpg"
+            >
+              v
+            </Link>
+            <Link
+              className="inline-flex text-xs dark:text-zinc-50"
+              href="/images/y.jpg"
+            >
+              y
+            </Link>{" "}
+            says hi!
+          </p>
+        </sub>
+      </footer>
+
+      {hearts.map((heart) => (
+        <span
+          key={heart.id}
+          className="love heart-animation"
+          style={{
+            left: heart.x,
+            top: heart.y + Math.floor(Math.random() * (501 - 451) + 450),
+          }}
+        >
+          ❤️
+        </span>
+      ))}
+    </>
   );
 }
