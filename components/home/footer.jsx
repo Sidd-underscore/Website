778ef8555270b769bf40db4736b34ef5c0af22c5
Sidd <siddharth726@outlook.com>
2024-08-20 21:34:32 -0700

    [FIX] fix footer layout for different screen widths oopsies

--- components/home/footer.jsx
+++ components/home/footer.jsx
@@ -2,43 +2,29 @@ import { Link } from "@/components/ui/link";
 
 export function Footer() {
   return (
-    <div className="relative w-screen">
-      <div className="z-10 absolute -top-[3.6rem] right-0 h-[3rem] w-[40vw] origin-bottom-right rotate-[358deg] border-t border-neutral-400 bg-neutral-50 dark:opacity-75 dark:border-neutral-700 dark:bg-neutral-900" />
-      <div className="z-10 absolute -top-4 right-0 h-[3rem] w-[12rem] origin-bottom-right bg-neutral-50 dark:opacity-75 dark:bg-neutral-900" />
+<div className="relative w-full">
+  {/* Sloped elements */}
+  <div className="absolute top-0 right-0 h-[6vh] w-[40vw] origin-bottom-right rotate-[1deg] border-t border-neutral-400 bg-neutral-50 dark:bg-neutral-900 dark:border-neutral-700" />
+  <div className="absolute top-0 right-0 h-[6vh] w-[15vw] origin-bottom-right bg-neutral-50 dark:bg-neutral-900" />
 
-      <div className="absolute -top-12 left-0 z-20 h-12 w-screen origin-bottom-left rotate-[2deg] border-t border-neutral-400 bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-900" />
+  <div className="absolute top-0 left-0 h-[6vh] w-full origin-bottom-left rotate-[2deg] border-t border-neutral-400 bg-neutral-100 dark:bg-neutral-900 dark:border-neutral-700" />
 
-      <footer className="z-0 relative h-48 items-center justify-around bg-neutral-100 p-4 text-center dark:bg-neutral-900 md:flex">
-        <div className="mx-auto w-fit">
-          <SiddWordMark className="" />
-        </div>
-
-        <div className="pb-4 text-xs md:mx-auto md:mb-0">
-          <p> Made with Next.js and love by Sidd in Portland, OR.</p>
-          <p>
-            <Link
-              className="inline-flex text-xs dark:text-neutral-50"
-              href="/images/i.jpg"
-            >
-              I
-            </Link>
-            <Link
-              className="inline-flex text-xs dark:text-neutral-50"
-              href="/images/v.jpg"
-            >
-              v
-            </Link>
-            <Link
-              className="inline-flex text-xs dark:text-neutral-50"
-              href="/images/y.jpg"
-            >
-              y
-            </Link>{" "}
-            says hi!
-          </p>
-        </div>
-      </footer>
+  {/* Footer */}
+  <footer className="relative z-10 h-48 flex items-center justify-around bg-neutral-100 p-4 text-center dark:bg-neutral-900 md:flex-row flex-col">
+    <div className="mx-auto w-fit">
+      <SiddWordMark className="" />
     </div>
+    <div className="pb-4 text-xs md:mx-auto md:mb-0">
+      <p>Made with Next.js and love by Sidd in Portland, OR.</p>
+      <p>
+        <Link className="inline-flex text-xs dark:text-neutral-50" href="/images/i.jpg">I</Link>
+        <Link className="inline-flex text-xs dark:text-neutral-50" href="/images/v.jpg">v</Link>
+        <Link className="inline-flex text-xs dark:text-neutral-50" href="/images/y.jpg">y</Link> says hi!
+      </p>
+    </div>
+  </footer>
+</div>
+
   );
 }
 
