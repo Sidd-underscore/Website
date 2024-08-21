2375698faf466b9025ea69471e8d87a0a993ded3
Sidd_ <siddharth726@outlook.com>
2024-08-21 00:49:54 -0700

    [FEAT] updated the Sidd logo yay and changed footer design

--- app/page.jsx
+++ app/page.jsx
@@ -4,24 +4,18 @@ import { Link } from "@/components/ui/link";
 import { FeaturedPhotos } from "@/components/photos/featured";
 import { Work } from "@/components/home/work";
 import { Achievements } from "@/components/home/achievements";
-import { Dancing_Script } from "next/font/google";
-
-const dancingScript = Dancing_Script({
-  subsets: ["latin"],
-  display: "swap",
-});
 
 export default function Home() {
   return (
     <div className="w-full text-left">
       <p>Hi, I&apos;m</p>
-      <h1 className="font-bold space-x-1 leading-none [background-size:400%_400%] bg-clip-text text-transparent animate-background-move bg-gradient-to-r from-pink-400 dark:from-pink-500 via-purple-300 dark:via-purple-500 to-blue-400 dark:to-blue-600">
-        <span className={dancingScript.className + " text-[16rem]"}>S</span>
-        <span className="text-[8rem]">idd</span>
+      <h1 className="space-x-1 text-9xl font-bold text-pink-300 pl-2">
+        sidd
       </h1>
 
       <p className="mt-12 items-center sm:flex">
-        I code, film videos, take photos, enjoy working light boards, and live stream.{" "}
+        I code, film videos, take photos, enjoy working light boards, and live
+        stream.{" "}
         <strong className="group cursor-pointer md:ml-2">
           <Link
             href="/about"
@@ -36,8 +30,6 @@ export default function Home() {
       <Work />
       <Achievements />
       <FeaturedPhotos />
-
-      
     </div>
   );
 }
