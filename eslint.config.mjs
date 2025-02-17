de7bb577d66e63b89f43ac677ffd3d7cd27acf9f
Sidd_ <hello@sidd.studio>
2025-02-17 02:07:27 -0800

    [FIX] images not being found by Vercel?

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,16 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.config({
-    extends: ["next", "prettier", "next/core-web-vitals"],
-  }),
+  ...compat.config({ extends: ["next", "prettier", "next/core-web-vitals"] }),
 ];
 
 export default eslintConfig;
