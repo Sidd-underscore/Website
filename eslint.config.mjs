8e4b9e951165bb563299807dce9d2d1c2b3b582c
Sidd_ <hello@sidd.studio>
2026-02-16 09:37:52 -0800

    Add project code review notes and fix lint command setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import prettier from "eslint-config-prettier";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.config({ extends: ["next", "prettier", "next/core-web-vitals"] }),
-];
+const eslintConfig = [...nextVitals, prettier];
 
 export default eslintConfig;
